460e5cfa9d93aeade4754e2017500e83a8b6ff97
Paul Frazee <pfrazee@gmail.com>
2021-03-14 15:14:20 -0500

    Add schemas and index pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("webfonts");
+
+  eleventyConfig.addLiquidFilter("prettyjson", function(value) {
+    return JSON.stringify(value, null, 2)
+  });
 };
